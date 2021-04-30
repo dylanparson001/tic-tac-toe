ed3259107323eb60ece4709eaf07ea1f19e31695
Dylan Parson <dylan.parson001@gmail.com>
2021-04-30 18:35:00 -0400

    beginning functionality for checking for win conditions

--- script.js
+++ script.js
@@ -34,6 +34,15 @@ const Player = (name) => {
 //module to create game board
 const gameBoard = (() => {
   const board = new Array(9);
+  const zero = board[0];
+  const one = board[1];
+  const two = board[2];
+  const three = board[3];
+  const four = board[4];
+  const five = board[5];
+  const six = board[6];
+  const seven = board[7];
+  const eight = board[8];
   const container = document.querySelector("#game-container");
 
   const createBoard = () => {
@@ -47,11 +56,55 @@ const gameBoard = (() => {
   };
 
   const playerChoice = (choice, symbol) => {
-    if (board[choice.id] === "") { // checks if space has been chosen
+    if (board[choice.id] === "") {
+      // checks if space has been chosen
       board[choice.id] = symbol;
       console.log(board);
     }
   };
+
+  //check for win, or tie
+  const checkWin = () => {
+    const result;
+
+    result = _checkRows();
+  }; // end checkWin
+
+  const _checkRows = () => {
+    if (zero === one && zero === two) {
+      // checks first row
+      if (zero === "x") {
+        return "x";
+      } if(zero === 'o') {
+        return "o";
+      }
+      return 'draw';
+    }
+    if (three === four && three === five) {
+      // checks first row
+      if (three === "x") {
+        return "x";
+      } if (three === 'o') {
+        return "o";
+      }
+      else {
+        return 'draw';
+      }
+    }
+    if (six === seven && six === eight) {
+      // checks first row
+      if (six === "x") {
+        return "x";
+      } if (six === 'o') {
+        return "o";
+      }
+      else {
+        return 'draw';
+      }
+    }
+
+  }; // end _checkRows
+
   return {
     createBoard,
     playerChoice,
@@ -69,8 +122,13 @@ const gameController = (() => {
     const container = document.getElementById("players");
     const firstPlayer = document.createElement("h2");
     const secondPlayer = document.createElement("h2");
+
+    firstPlayer.id = "player-1";
+    secondPlayer.id = "player-2";
+
     firstPlayer.textContent = `Player 1: ${player1.getScore()}`;
     secondPlayer.textContent = `Player 2: ${player2.getScore()}`;
+
     container.appendChild(firstPlayer);
     container.appendChild(secondPlayer);
   };
@@ -85,8 +143,10 @@ const gameController = (() => {
 
     spaceList.forEach((space) => {
       space.addEventListener("click", () => {
-        if (turnCounter === 1) { // checks which player's turn
-          if (space.textContent === "") { // checks if that space has been chosen
+        if (turnCounter === 1) {
+          // checks which player's turn
+          if (space.textContent === "") {
+            // checks if that space has been chosen
             space.textContent = player1.getSymbol(); //testing
             gameBoard.playerChoice(space, player1.getSymbol());
             turnCounter = 2;
